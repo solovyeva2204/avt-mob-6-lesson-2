d7d0311181bd71102a969fa6a49922a69aa212c3
Ekaterina Solovyeva <e.solovyeva@bk.ru>
2018-12-01 18:38:20 +0300

    Lesson 3
    Ex 2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -79,6 +79,58 @@ public class FirstTest
 
     }
 
+    @Test
+    public void TestCancelSearch()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' element",
+                5
+
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains (@text, 'Search…')]"),
+                "iOS",
+                "Cannot find search element",
+                5
+
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Mobile operating system by Apple']"),
+                "Cannot find topic 'iOS' searching by iOS",
+                15
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Eleventh major release of iOS, the mobile operating system by Apple Inc.']"),
+                "Cannot find topic 'iOS 11' searching by iOS",
+                10
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Tenth major release of iOS, the mobile operating system by Apple Inc.']"),
+                "Cannot find topic 'iOS 10' searching by iOS",
+                5
+        );
+
+
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Cannot find X for cancel searching results",
+                15
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/search_empty_message"),
+                "Cannot find search empty element",
+                5
+        );
+
+    }
+
 
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
 {
