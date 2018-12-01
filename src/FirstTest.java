a5069e4bf96fca1d1b0bf5953812a89f51676624
Ekaterina Solovyeva <e.solovyeva@bk.ru>
2018-12-01 17:40:41 +0300

    lesson 3
    Ex. 1

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,15 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -12,6 +18,7 @@ public class FirstTest
     private AppiumDriver driver;
 
     @Before
+
     public void setUp() throws Exception
 
     {
@@ -34,9 +41,72 @@ public class FirstTest
         driver.quit();
     }
 
-    @Test
-    public  void firstTest()
+
+   @Test
+    public  void TestCompareSearchLabel()
     {
-        System.out.println("First test run");
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find element",
+                5
+
+        );
+
+        WebElement search_label = waitForElementPresent(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find search element",
+                5
+        );
+
+
+
+        String element_label = search_label.getAttribute("text");
+        Assert.assertEquals(
+                "We see unexpected label",
+                "Search…",
+                element_label
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains (@text, 'Search…')]"),
+                "Java",
+                "Cannot find element",
+                5
+
+        );
+
+
+
     }
+
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
+{
+    WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+    wait.withMessage(error_message + "\n");
+    return wait.until(
+            ExpectedConditions.presenceOfElementLocated(by)
+    );
+}
+    private WebElement waitForElementPresent(By by, String error_message)
+    {
+        return waitForElementPresent(by, error_message, 5);
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeOutAndSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeOutAndSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeOutAndSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeOutAndSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+
+
 }
